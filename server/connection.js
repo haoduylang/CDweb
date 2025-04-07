c8a2ea497dbbd7d4c2491e2ad785800574675157
haoduylang <21130347@st.hcmuaf.edu.vn>
2025-04-07 12:35:24 +0700

    Remove .env from tracking

--- server/connection.js
+++ server/connection.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-const url = `mongodb+srv://haolang:abc123456@haolang.ylwnt.mongodb.net/`;
+const url = process.env.MONGO_URI;
 
-mongoose.connect(url).then(() => console.log('Kết nối thành công'))
-.catch(err => console.error('Lỗi kết nối:', err));
+mongoose.connect(url)
+  .then(() => console.log('Kết nối thành công'))
+  .catch(err => console.error('Lỗi kết nối:', err));
